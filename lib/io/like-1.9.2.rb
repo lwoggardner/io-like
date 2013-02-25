315ae394444641fd3d8d8f1d7af21a0e33bb0240
Grant Gardner <grant@lastweekend.com.au>
2013-02-25 23:30:54 +1100

    Byte based methods for 1.9

--- lib/io/like-1.9.2.rb
+++ lib/io/like-1.9.2.rb
@@ -5,6 +5,15 @@ class IO
     include IO::Like_1_8_7
 
     # call-seq:
+    #   ios.bytes            -> anEnumerator
+    #
+    # Calls #each_byte without a block and returns the resulting
+    # <code>Enumerator</code> instance.
+    def bytes
+      self.to_enum(:each_byte)
+    end
+
+    # call-seq:
     #   ios.close_on_exec?  -> raise NotImplementedError
     def close_on_exec?()
       raise NotImplementedError
@@ -14,6 +23,57 @@ class IO
     #   ios.close_on_exec=  -> raise NotImplementedError
     def close_on_exec=(coe)
       raise NotImplementedError
+    end
+
+    # call-seq:
+    #   ios.each_byte { |char| block } -> ios
+    #   ios.each_byte        -> anEnumerator
+    #
+    # Reads each byte from the stream and calls the given block once for
+    # each character, passing the byte as an argument.
+    #
+    # When called without a block, returns an instance of
+    # <code>Enumerator</code> which will iterate over each byte
+    # in the same manner.
+    #
+    # <b>NOTE:</b> This method ignores <code>Errno::EAGAIN</code> and
+    # <code>Errno::EINTR</code> raised by #unbuffered_read.  Therefore, this
+    # method always blocks.  Aside from that exception and the conversion of
+    # <code>EOFError</code> results into <code>nil</code> results, this method
+    # will also raise the same errors and block at the same times as
+    # #unbuffered_read.
+    def each_byte
+      unless block_given?
+        self.to_enum(:each_byte)
+      else
+        while (byte = getbyte)
+          yield byte
+        end
+        self
+      end
+    end
+
+    # call-seq:
+    #   ios.getbyte       -> fixnum or nil
+    #
+    # Calls #readbyte and either returns the result or <code>nil</code> if
+    # on <code>EOFError</code>.
+    #
+    # Raises <code>IOError</code> if #closed? returns <code>true</code>.  Raises
+    # <code>IOError</code> unless #readable? returns <code>true</code>.  Raises
+    # all errors raised by #unbuffered_read except for <code>EOFError</code>.
+    #
+    # <b>NOTE:</b> This method ignores <code>Errno::EAGAIN</code> and
+    # <code>Errno::EINTR</code> raised by #unbuffered_read.  Therefore, this
+    # method always blocks.  Aside from that exception and the conversion of
+    # <code>EOFError</code> results into <code>nil</code> results, this method
+    # will also raise the same errors and block at the same times as
+    # #unbuffered_read.
+    # nil at eof
+    def getbyte
+      readbyte()
+    rescue EOFError
+      nil
     end
 
     # call-seq:
@@ -58,6 +118,23 @@ class IO
         write(ors) if string.nil? || string.index(ors, -ors.length).nil?
       end
       nil
+    end
+
+    # call-seq:
+    #   ios.readbyte         -> fixnum
+    #
+    # Returns the next 8-bit byte (0..255) from the stream.
+    #
+    # Raises <code>EOFError</code> when there is no more data in the stream.
+    # Raises <code>IOError</code> if #closed? returns <code>true</code>.  Raises
+    # <code>IOError</code> unless #readable? returns <code>true</code>.
+    #
+    # <b>NOTE:</b> This method ignores <code>Errno::EAGAIN</code> and
+    # <code>Errno::EINTR</code> raised by #unbuffered_read.  Therefore, this
+    # method always blocks.  Aside from that exception, this method will also
+    # raise the same errors and block at the same times as #unbuffered_read.
+    def readbyte
+      __io_like__buffered_read(1).getbyte(0)
     end
 
     # call-seq:
@@ -120,6 +197,29 @@ class IO
     end
 
     # call-seq:
+    #   ios.ungetbyte(string)  -> nil
+    #   ios.ungetbyte(integer) -> nil
+    #
+    # A string argument is forced to 'binary' encoding and then
+    # passed on to #unread.
+    #
+    # The low byte of an integer argument is converted to a binary
+    # string and passed on to #unread.
+    #
+    #TODO rubyspecs for ungetbyte
+    def ungetbyte(string)
+      raise IOError, 'closed stream' if closed?
+      raise IOError, 'not opened for reading' unless readable?
+      if string.respond_to?(:to_int)
+        int = string.to_int & 0xFF
+        __io_like__unread(int.chr(Encoding::BINARY))
+      else
+        __io_like__unread(string)
+      end
+      nil
+    end
+
+    # call-seq:
     #   ios.write_nonblock(string)    -> integer
     #
     # The argument is converted to a string with <code>to_s</code>, converted
@@ -142,6 +242,33 @@ class IO
         ex.extend(IO::WaitWritable)
         raise ex
       end
+    end
+
+    private
+
+    # force string to binary and insert back into the read buffer
+    # TODO: Find out why #unread is not a private method for IO::Like
+    def __io_like__unread(string)
+      enc = string.encoding
+      string.force_encoding(Encoding::BINARY)
+      begin
+        __io_like__internal_read_buffer.insert(0, string)
+      ensure
+        string.force_encoding(enc)
+      end
+      nil
+    end
+
+    # Returns a reference to the internal read buffer which is always
+    # a binary encoded string.
+    def __io_like__internal_read_buffer
+      @__io_like__read_buffer ||= String.new('').force_encoding(Encoding::BINARY)
+    end
+
+    # Returns a reference to the internal write buffer which is always
+    # a binary encoded string.
+    def __io_like__internal_write_buffer
+      @__io_like__write_buffer ||= String.new('').force_encoding(Encoding::BINARY)
     end
 
   end
